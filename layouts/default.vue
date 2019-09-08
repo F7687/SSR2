f9c3f18da339a114ad12e1a38404ac792faf288f
7687 <44029060@qq.com>
2019-09-09 01:04:00 +0800

    重修后的ssr

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,28 @@
 <template>
   <div>
+    <!-- 头部组件 -->
+    <Header/>
+    <!-- 内容占位组件 -->
     <nuxt />
+    <!-- 底部组件 -->
+    <Footer/>
   </div>
 </template>
 
-<style>
+<script>
+import Footer from '@/components/footer.vue'
+import Header from '@/components/header.vue'
+export default {
+  components:{
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style lang="less">
 html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
     Roboto, 'Helvetica Neue', Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
@@ -17,39 +33,28 @@ html {
   box-sizing: border-box;
 }
 
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
+*{
+  margin:0; 
+  padding:0;
 }
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+ul, li, ol{
+  list-style:none;
 }
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
+a{
+  text-decoration:none;
+  color:inherit;
 }
+a:hover{
 
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
 }
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+em,i{
+  font-style: normal;
+}
+.icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
 }
 </style>
